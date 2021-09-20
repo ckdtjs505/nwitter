5673bfe49c821b15a5f3b92218c1fd3ccf8d23cd
ckdtjs505 <ckdtjs505@naver.com>
2021-09-20 12:38:55 +0900

    update sign in and login email password

--- src/route/Auth.tsx
+++ src/route/Auth.tsx
@@ -1,8 +1,17 @@
-import { signInWithPopup } from "@firebase/auth";
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup
+} from "@firebase/auth";
 import { firebaseAuth, googleLoginProvider } from "firebase";
 
-const Auth = ({ setIsLogin } : {setIsLogin : any}) => {
-  const onClick = () => {
+const Auth = ({ setIsLogin }: { setIsLogin: any }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 소셜 로그인 버튼 클릭시
+  const onClickSocialLogin = () => {
     signInWithPopup(firebaseAuth, googleLoginProvider)
       .then(result => {
         console.log(result);
@@ -13,10 +22,66 @@ const Auth = ({ setIsLogin } : {setIsLogin : any}) => {
       });
   };
 
+  // 아이디 비밀번호 입력시
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    if (e.target.name === "elEmail") {
+      setEmail(e.target.value);
+    } else if (e.target.name === "elPassword") {
+      setPassword(e.target.value);
+    }
+  };
+
+  // 로그인, 계정 생성 버튼
+  const hanldeSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      name: string;
+      email: { value: string };
+      password: { value: string };
+    };
+
+    // 계정 생성
+    if (target.name === "signIn") {
+      createUserWithEmailAndPassword(firebaseAuth, email, password).catch(err => {
+        console.log(err);
+      });
+    } else {
+      // 로그인
+      signInWithEmailAndPassword(firebaseAuth, email, password);
+    }
+  };
+
   return (
     <>
       <div>auth</div>
-      <button name="Goggle" onClick={onClick}>
+      <form onSubmit={hanldeSubmit}>
+        <input
+          value={email}
+          name="elEmail"
+          type="email"
+          onChange={handleInput}
+          placeholder={"아이디를 입력하세요"}
+          required
+        ></input>
+        <input
+          value={password}
+          name="elPassword"
+          type="password"
+          onChange={handleInput}
+          placeholder={"비밀번호를 입력하세요"}
+          required
+        ></input>
+        <button name="login" type="submit">
+          Login
+        </button>
+        <button name="signIn" type="submit">
+          Sign in
+        </button>
+      </form>
+
+      <button name="Goggle" onClick={onClickSocialLogin}>
         Google Login
       </button>
     </>
