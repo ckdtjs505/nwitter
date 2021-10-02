6eb5fbc15b8083178558e4ed4b7a31638ef7b9f9
ckdtjs505 <ckdtjs505@naver.com>
2021-10-02 10:55:49 +0900

    update nweet form and img box

--- src/components/NweetsForm.tsx
+++ src/components/NweetsForm.tsx
@@ -4,6 +4,66 @@ import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 
 import React, { useState } from "react";
 import { addDoc } from "@firebase/firestore";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  padding: 10px;
+  border-bottom: rgb(239, 243, 244) 1px solid;
+`;
+
+const Img = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 5rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+`;
+
+const FileInput = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+`;
+
+const Input = styled.input`
+  border: none;
+  margin-top: 1rem;
+  font-size: 20px;
+`;
+
+const FormFooter = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 1rem;
+  padding-top: 1rem;
+  border-top: rgb(239, 243, 244) 1px solid;
+`;
+
+const FileFormImg = styled.div`
+  width: 25px;
+`;
+
+const NweetBtn = styled.button`
+  background-color: #1d9bf0;
+  color: white;
+  height: 36px;
+  width: 75.9688px;
+  font-weight: 700;
+  font-size: 15px;
+  border: none;
+  border-radius: 2rem;
+`;
+
+const Content = styled.div`
+  width: 100%;
+`;
 
 const NweetsFrom = ({ user }: any) => {
   const [text, setText] = useState("");
@@ -60,23 +120,40 @@ const NweetsFrom = ({ user }: any) => {
     setUploadFile(null);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={text}
-        onChange={handleChange}
-        type="text"
-        placeholder="아무거나 입력해"
-        maxLength={120}
-      ></input>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
-      {uploadFile && (
-        <div>
-          <img src={uploadFile} width="50px" height="50px" />
-          <button onClick={handleDeleteImg}> 이미지 취소</button>
-        </div>
-      )}
-      <button type="submit">제출</button>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      <Img src={user.photoURL} />
+
+      <Content>
+        <Input
+          value={text}
+          onChange={handleChange}
+          type="text"
+          placeholder="What's happening?"
+          maxLength={120}
+        ></Input>
+        {uploadFile && (
+          <div>
+            <img src={uploadFile} width="50px" height="50px" />
+            <button onClick={handleDeleteImg}> 이미지 취소</button>
+          </div>
+        )}
+        <FormFooter>
+          <FileFormImg>
+            <label htmlFor="upload">
+              <svg viewBox="0 0 24 24">
+                <g>
+                  <path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"></path>
+                  <circle cx="8.868" cy="8.309" r="1.542"></circle>
+                </g>
+              </svg>
+            </label>
+            <FileInput id="upload" type="file" accept="image/*" onChange={handleFileChange} />
+          </FileFormImg>
+
+          <NweetBtn type="submit">Nweet</NweetBtn>
+        </FormFooter>
+      </Content>
+    </Form>
   );
 };
 
