629d54eb6b6a02776bfe893d7d4743e6e45aa90e
ckdtjs505 <ckdtjs505@naver.com>
2021-10-24 22:58:18 +0900

    feat: code split :

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,41 +1,44 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "route/auth/index";
-import Home from "route/home/index";
-import Profile from "route/profile/Profile";
-import Navigator from "components/navigator/Navigator";
-import { useContext } from "react";
+import { useContext, lazy, Suspense } from "react";
 import { AuthContext } from "context/context";
 import Nweet from "route/nweet";
+
+const Home = lazy(() => import("route/home/index"));
+const Auth = lazy(() => import("route/auth/index"));
+const Profile = lazy(() => import("route/profile/Profile"));
+const Navigator = lazy(() => import("components/navigator/Navigator"));
 
 const Routes = () => {
   const userInfo = useContext(AuthContext);
   return (
-    <Router>
-      {userInfo?.init && userInfo?.isLogin ? (
-        <>
-          <Navigator />
+    <Suspense fallback={<div>loading...</div>}>
+      <Router>
+        {userInfo?.init && userInfo?.isLogin ? (
+          <>
+            <Navigator />
 
-          <Switch>
-            <Route exact path="/profile">
-              <Profile></Profile>
-            </Route>
-            <Route exact path="/home">
-              <Home />
-            </Route>
+            <Switch>
+              <Route exact path="/profile">
+                <Profile></Profile>
+              </Route>
+              <Route exact path="/home">
+                <Home />
+              </Route>
 
-            <Route exact path="/:userId/:createTime">
-              <Nweet />
-            </Route>
+              <Route exact path="/:userId/:createTime">
+                <Nweet />
+              </Route>
 
-            <Route path="*">
-              <Home />
-            </Route>
-          </Switch>
-        </>
-      ) : (
-        <Auth />
-      )}
-    </Router>
+              <Route path="*">
+                <Home />
+              </Route>
+            </Switch>
+          </>
+        ) : (
+          <Auth />
+        )}
+      </Router>
+    </Suspense>
   );
 };
 
