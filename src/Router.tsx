197603eaf063db253477f3bd904ca5fd75e92493
ckdtjs505 <ckdtjs505@naver.com>
2021-10-25 23:19:06 +0900

    feat : create error boundary

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { useContext, lazy, Suspense } from "react";
 import { AuthContext } from "context/context";
 import Nweet from "route/nweet";
+import ErrorBoundary from "components/ErrorBoundary";
 
 const Home = lazy(() => import("route/home/index"));
 const Auth = lazy(() => import("route/auth/index"));
@@ -10,35 +11,37 @@ const Navigator = lazy(() => import("components/navigator/Navigator"));
 
 const Routes = () => {
   const userInfo = useContext(AuthContext);
+
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Router>
-        {userInfo?.init && userInfo?.isLogin ? (
-          <>
-            <Navigator />
+    <ErrorBoundary>
+      <Suspense fallback={<div>loading...</div>}>
+        <Router>
+          {userInfo?.init && userInfo?.isLogin ? (
+            <>
+              <Navigator />
+              <Switch>
+                <Route exact path="/profile">
+                  <Profile></Profile>
+                </Route>
+                <Route exact path="/home">
+                  <Home />
+                </Route>
 
-            <Switch>
-              <Route exact path="/profile">
-                <Profile></Profile>
-              </Route>
-              <Route exact path="/home">
-                <Home />
-              </Route>
+                <Route exact path="/:userId/:createTime">
+                  <Nweet />
+                </Route>
 
-              <Route exact path="/:userId/:createTime">
-                <Nweet />
-              </Route>
-
-              <Route path="*">
-                <Home />
-              </Route>
-            </Switch>
-          </>
-        ) : (
-          <Auth />
-        )}
-      </Router>
-    </Suspense>
+                <Route path="*">
+                  <Home />
+                </Route>
+              </Switch>
+            </>
+          ) : (
+            <Auth />
+          )}
+        </Router>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
